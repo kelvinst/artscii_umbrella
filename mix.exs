f2fee34a3a29b3de8c4bf274def93246a3593d7e
kelvinst <kelvin.stinghen@gmail.com>
2021-03-03 11:20:13 -0300

    Adding credo to the project
    I like it cause sometimes it gets some interesting issues for me :D

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule Artscii.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [ 
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
