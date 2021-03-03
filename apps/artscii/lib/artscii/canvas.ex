f07283f6d12478960dd518709a55f980c1d2f65e
kelvinst <kelvin.stinghen@gmail.com>
2021-03-02 21:39:45 -0300

    Store behaviour
    The idea is to have a persistence layer separated from the actual
    business logic and data structure. Which would allow to test it using
    mocked persistence, or even have multiple persistence strategies in the
    future.

--- apps/artscii/lib/artscii/canvas.ex
+++ apps/artscii/lib/artscii/canvas.ex
@@ -4,5 +4,13 @@ defmodule Artscii.Canvas do
   operations.
   """
 
+  alias Artscii.Canvas
+
+  @typedoc "The identifier of the canvas"
+  @type id :: String.t()
+
+  @typedoc "The canvas struct"
+  @type t :: %Canvas{id: id()}
+
   defstruct [:id]
 end
---
+++ apps/artscii/lib/artscii/store.ex
@@ -0,0 +1,42 @@
+defmodule Artscii.Store do
+  @moduledoc """
+  Defines a persistence store.
+
+  The persistence store is the responsible for saving
+  the canvases somewhere. For example, one option is to save the 
+  canvases to ETS tables, so the module should implement this 
+  behaviour.
+  """
+
+  alias Artscii.Canvas
+
+  @doc """
+  Inits the persistence layer.
+  """
+  @callback init() :: :ok
+
+  @doc """
+  Creates the canvas.
+
+  Returns `{:ok, canvas}` if it works, where `canvas` is the newly saved value,
+  or `{:error, :already_exists}` if a canvas with the same id already exists.
+  """
+  @callback create(Canvas.t()) :: {:ok, Canvas.t()} | {:error, :already_exists}
+
+  @doc """
+  Saves the canvas.
+
+  If a canvas with the given id already exist, it updates it,
+  if not, a new one is inserted.
+
+  Always returns `{:ok, canvas}` where `canvas` is the newly saved value,
+  """
+  @callback save(Canvas.t()) :: {:ok, Canvas.t()}
+
+  @doc """
+  Fetch the canvas by it's `id`.
+
+  Returns `{:ok, canvas}` if found, or `{:error, :not_found}` if not
+  """
+  @callback fetch(Canvas.id()) :: {:ok, Canvas.t()} | {:error, :not_found}
+end
