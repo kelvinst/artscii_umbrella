98a3c675fa3dec9c7f4d7fef57841be42a3ccee2
kelvinst <kelvin.stinghen@gmail.com>
2021-03-04 17:06:34 -0300

    Fixing ArtsciiWeb doc
    The module changed completely, so we have to update the docs

--- apps/artscii_web/lib/artscii_web.ex
+++ apps/artscii_web/lib/artscii_web.ex
@@ -1,18 +1,5 @@
 defmodule ArtsciiWeb do
   @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use ArtsciiWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
+  This is the app the holds the GraphQL schema
   """
 end
