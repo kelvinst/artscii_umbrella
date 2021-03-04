ab9d4a10b5ea9343d35e2f2464f37996bd4c607d
kelvinst <kelvin.stinghen@gmail.com>
2021-03-04 17:13:09 -0300

    Improving a bit more the docs
    The docs for the error templates were not an actual doc, just a comment

--- apps/artscii_web/lib/artscii_web/views/error_view.ex
+++ apps/artscii_web/lib/artscii_web/views/error_view.ex
@@ -5,15 +5,13 @@ defmodule ArtsciiWeb.ErrorView do
 
   use Phoenix.View, root: "lib/artscii_web/templates", namespace: ArtsciiWeb
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  @doc """
+  This is the function called when no template is found.
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  By default, it returns the status message from
+  the template name. For example, "404.json" becomes
+  "Not Found".
+  """
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
